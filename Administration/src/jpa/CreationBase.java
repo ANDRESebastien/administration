7db1a05691cbd1e694be863edd0626d8898f1e27
ANDRESebastien <andreseba_3@hotmail.com>
2018-01-19 10:42:07 +0100

    init base

--- Administration/src/jpa/CreationBase.java
+++ Administration/src/jpa/CreationBase.java
@@ -6,28 +6,37 @@ import java.sql.Statement;
 
 public class CreationBase {
 
+	/*
 	public final static String[] QUERIES = {
 			"drop table Administration if exists",
-			"create table Administration (id bigint not null, nom varchar(255), motDePasse varchar(255), primary key (id))",
+			"create table Administration (id bigint not null, nom varchar(255), motDePasse varchar(255), primary key (id))"
 	};
+	*/
 
 	public static void main(String[] args) throws Exception {
+		
+		System.out.println("debut");
 
 		Class.forName("org.hsqldb.jdbcDriver").getConstructor().newInstance();
-		//  java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:/data/basejpa --dbname.0 basejpa
+		//  java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:C:/git/Administration/Administration/data/baseAdministration --dbname.0 baseAdministration
 		
 		Connection connexion = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9001/baseAdministration", "sa",  "");
 		
 		Statement instructionSQL = connexion.createStatement();
+		
+		
+		String sql = "create table Administration (id bigint not null, nom varchar(255), motDePasse varchar(255), primary key (id))";
+		instructionSQL.executeUpdate(sql);
 
+		/*
 		for (String query : QUERIES) {
 			System.out.println(query);
 			instructionSQL.executeUpdate(query);
 		}
+		*/
 
 		instructionSQL.close();
 		connexion.close();
 
 	}
-
 }
